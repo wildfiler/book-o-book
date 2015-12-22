5455c6c48c37123929d79bedc58a1d10604c6336
Alexey Neischenko <alexneisc@gmail.com>
2015-12-23 00:22:39 +0200

    Add association of books and authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ActiveRecord::Base
+  has_many :books
+
   def last_5_books
     ['http://files2.adme.ru/files/news/part_41/410605/4537355-R3L8T8D-600-031.jpg',
      'http://files5.adme.ru/files/news/part_41/410605/4537805-R3L8T8D-600-4173383.jpg',
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ActiveRecord::Base
+  belongs_to :author
+
   paginates_per 20
 end
