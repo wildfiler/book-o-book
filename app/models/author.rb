0288e25d7193b81b6d11c0bc21dec538005af127
Alexey Neischenko <alexneisc@gmail.com>
2015-12-23 20:17:40 +0200

    Change #last_5_books. Get real books, not dummy

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,7 @@
 class Author < ActiveRecord::Base
   has_many :books
 
-  def last_5_books
-    ['http://files2.adme.ru/files/news/part_41/410605/4537355-R3L8T8D-600-031.jpg',
-     'http://files5.adme.ru/files/news/part_41/410605/4537805-R3L8T8D-600-4173383.jpg',
-     'http://files6.adme.ru/files/news/part_41/410605/4537905-R3L8T8D-600-8112228.jpg',
-     'http://files8.adme.ru/files/news/part_41/410605/4538005-R3L8T8D-600-9654148.jpg',
-     'http://www.cornellpubs.com/Images2/Ideal%201949%20no36.jpg'].shuffle
+  def last_5_books author
+    author.books.order(published_at: :desc).first 5
   end
 end
