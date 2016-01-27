b76c2539f7fd1f70834ecf5472d4bc88272b8d0f
Fatpaher <fatpaher@gmail.com>
2016-01-27 12:14:41 +0200

    Hound cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,7 @@ class UsersController < ApplicationController
   end
 
   private
-
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
