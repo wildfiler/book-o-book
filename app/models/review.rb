13d27b003c70bcbac47475523f1ec07578e6ff9d
Fatpaher <fatpaher@gmail.com>
2016-02-02 20:34:52 +0200

    Add validation uniquness to review model throgh user_id and book_id

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ActiveRecord::Base
+  validates_uniqueness_of :user_id, scope: :book_id
   belongs_to :book
   belongs_to :user
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,4 +5,9 @@ describe Review do
     it { should belong_to(:user) }
     it { should belong_to(:book) }
   end
+
+
+  describe "User can leave only one review to book" do
+    it { should validate_uniqueness_of(:user_id).scoped_to(:book_id) }
+  end
 end
